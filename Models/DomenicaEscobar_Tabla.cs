572d9dfe6df692af5207bdf85a7b6414794fadad
Domenica Escobar <domenica.escobar.moyano@udla.edu.ec>
2023-10-25 07:44:11 -0600

    Explicacion codigo

--- Models/DomenicaEscobar_Tabla.cs
+++ Models/DomenicaEscobar_Tabla.cs
@@ -2,14 +2,18 @@
 
 namespace DomenicaEscobar_ExamenP1.Models
 {
+    // Definimos la clase DomenicaEscobar_Tabla
     public class DomenicaEscobar_Tabla
     {
+        //Identificar de tipo
         public int Id { get; set; }
         [Required]
         public string? DEAtributo1 { get; set; }
+        //Para un atributo numerico
         public bool DEAtributo2 { get; set; }
         [Range(5, 200)]
         public decimal DEAtributo3 { get; set; }
+        //Para poder colocar fecha
         public DateTime DEFecha { get; set; }
     }
 }
